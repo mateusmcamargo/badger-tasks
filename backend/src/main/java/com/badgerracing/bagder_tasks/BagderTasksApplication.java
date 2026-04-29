c2fb7267eaab4d42ce080de3509cf3fe6ff59454
mateus <mateuscamargo2908@gmail.com>
2026-04-29 15:07:44 -0300

    generated spring app with spring initializr

---
+++ backend/src/main/java/com/badgerracing/bagder_tasks/BagderTasksApplication.java
@@ -0,0 +1,13 @@
+package com.badgerracing.bagder_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagderTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagderTasksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/badgerracing/bagder_tasks/BagderTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badgerracing.bagder_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagderTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
