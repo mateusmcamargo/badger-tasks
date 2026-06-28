7c9862db798ff277980bfef1e8bb7147ab154afe
mateus <mateuscamargo2908@gmail.com>
2026-06-28 00:05:13 -0300

    add PATCH endpoint

--- backend/src/main/java/com/badgerracing/bagder_tasks/config/SecurityConfig.java
+++ backend/src/main/java/com/badgerracing/bagder_tasks/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
